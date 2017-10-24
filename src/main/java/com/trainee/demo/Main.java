c33ed199f5bef6f46806c208f6839d276c25f9c5
vadimka <zavertalyuk.v@gmail.com>
2017-10-24 10:10:15 +0300

    get root element

--- src/main/java/com/trainee/demo/Main.java
+++ src/main/java/com/trainee/demo/Main.java
@@ -1,7 +1,19 @@
 package com.trainee.demo;
 
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.input.SAXBuilder;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("HW!");
+    public static void main(String[] args) throws JDOMException, IOException {
+        File inputFile = new File("example.xml");
+        SAXBuilder saxBuilder = new SAXBuilder();
+        Document document = saxBuilder.build(inputFile);
+        Element classElement = document.getRootElement();
+        System.out.println(classElement);
     }
 }
