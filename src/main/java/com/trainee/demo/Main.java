da659a419ed7bcf46eb87729a6dcea779d34812a
vadimka <zavertalyuk.v@gmail.com>
2017-10-25 18:16:45 +0300

    add solution

--- src/main/java/com/trainee/demo/Main.java
+++ src/main/java/com/trainee/demo/Main.java
@@ -9,6 +9,10 @@ import org.xml.sax.SAXException;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -18,18 +22,17 @@ import java.util.List;
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) throws JDOMException, IOException, SAXException, ParserConfigurationException, URISyntaxException {
+    public static void main(String[] args) throws JDOMException, IOException, SAXException, ParserConfigurationException, URISyntaxException, XPathExpressionException {
         Map<Doc, String> map = new HashMap();
         List<String> parse = Parser.parse(Doc.REFERENCE.getxPath().get(0));
         File fXmlFile = new File("example.xml");
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-        Document doc = dBuilder.parse(fXmlFile);
-        doc.getDocumentElement().normalize();
 
-
-        System.out.println(doc.getElementsByTagName("Rate").item(1).getChildNodes().item(0).getNodeValue());
-//        showDocument(doc);
+        Document doc =dBuilder.parse(fXmlFile);
+        XPathExpression xp = XPathFactory.newInstance().newXPath().compile("/Placing/ContractSection/ContractMarket/PremiumRegulatoryAllocationScheme/Allocation/AllocationReference");
+        NodeList links = (NodeList) xp.evaluate(doc, XPathConstants.NODESET);
+        System.out.println(links.item(0).getChildNodes().item(0).getNodeValue());
     }
 
     private static void showDocument(Document doc) {
