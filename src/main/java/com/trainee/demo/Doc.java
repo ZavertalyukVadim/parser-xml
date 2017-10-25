5357dfef2efdf0b89e14844e2a83e03d3abaf7c8
vadimka <zavertalyuk.v@gmail.com>
2017-10-25 09:34:20 +0300

    change names to upper case

--- src/main/java/com/trainee/demo/Doc.java
+++ src/main/java/com/trainee/demo/Doc.java
@@ -5,31 +5,31 @@ import java.util.Arrays;
 import java.util.List;
 
 public enum Doc {
-    Reference("Reference","/Placing/ContractSection/ContractMarket/PremiumRegulatoryAllocationScheme/Allocation/AllocationReference"),
-    Country("Country","/Placing/ContractSection/RiskLocation/Location/Description"),
-    Insured("(Re) Insured","/Placing/InsuredDescription", "/Placing/OriginalPolicyholder/Party/Name"),
-    Description("Description","/Placing/ContractSection/ContractSectionClass/ClassOfBusinessDescription"),
-    BrokerId("Broker Id","/Placing/Broker/Party/Id"),
-    BrokerName("Broker Name","/Placing/Broker/Party/Name"),
-    PremiumCurrency("Premium Currency (1st textbox)","/Placing/ContractSection/Premium/PremiumAmount/AmtCcy"),
-    PremiumAmount("Premium Amount (2nd textbox)","/Placing/ContractSection/Premium/PremiumAmount/Amt"),
-    LimitCurrency("Limit Currency (1st textbox)","/Placing/ContractSection/ContractCoverage/CoverageAmount/AmtCcy"),
-    LimitAmount("Limit Amount (2nd textbox)","/Placing/ContractSection/ContractCoverage/CoverageAmount/Amt"),
-    Bkrage("Bkrage %","/Placing/ContractSection/Brokerage/BrokeragePercentage/Rate"),
-    Basis("Basis","/Placing/ContractSection/ContractCoverage/CoverageBasis "),
-    ContractStart("Contract Start","/Placing/ContractSection/ContractPeriod/StartDateTime"),
-    End("End","/Placing/ContractSection/ContractPeriod/EndDateTime"),
-    Written("Written","/Placing/ContractSection/ContractMarket/WrittenDateTime"),
-    Settle("Settle","/Placing/ContractSection/Premium/PaymentDate"),
-    Cancel("Cancel","/Placing/ContractSection/ContractPeriod/EndDateTime"),
+    REFERENCE("Reference","/Placing/ContractSection/ContractMarket/PremiumRegulatoryAllocationScheme/Allocation/AllocationReference"),
+    COUNTRY("Country","/Placing/ContractSection/RiskLocation/Location/Description"),
+    INSURED("(Re) Insured","/Placing/InsuredDescription", "/Placing/OriginalPolicyholder/Party/Name"),
+    DESCRIPTION("Description","/Placing/ContractSection/ContractSectionClass/ClassOfBusinessDescription"),
+    BROKEN_ID("Broker Id","/Placing/Broker/Party/Id"),
+    BROKEN_NAME("Broker Name","/Placing/Broker/Party/Name"),
+    PREMIUN_CURRENCY("Premium Currency (1st textbox)","/Placing/ContractSection/Premium/PremiumAmount/AmtCcy"),
+    PREMIUM_AMOUNT("Premium Amount (2nd textbox)","/Placing/ContractSection/Premium/PremiumAmount/Amt"),
+    LIMIT_CURRENCY("Limit Currency (1st textbox)","/Placing/ContractSection/ContractCoverage/CoverageAmount/AmtCcy"),
+    LIMIT_AMOUNT("Limit Amount (2nd textbox)","/Placing/ContractSection/ContractCoverage/CoverageAmount/Amt"),
+    BKRAGE("Bkrage %","/Placing/ContractSection/Brokerage/BrokeragePercentage/Rate"),
+    BASIS("Basis","/Placing/ContractSection/ContractCoverage/CoverageBasis "),
+    CONTRACT_START("Contract Start","/Placing/ContractSection/ContractPeriod/StartDateTime"),
+    END("End","/Placing/ContractSection/ContractPeriod/EndDateTime"),
+    WRITTEN("Written","/Placing/ContractSection/ContractMarket/WrittenDateTime"),
+    SETTLE("Settle","/Placing/ContractSection/Premium/PaymentDate"),
+    CANCEL("Cancel","/Placing/ContractSection/ContractPeriod/EndDateTime"),
     UMR("UMR","/Placing/Contract/BrokerReference"),
-    Ord("Ord %","/Placing/ContractSection/BrokerSharePercentage/Rate"),
-    Line("Line %","/Placing/ContractSection/ContractMarket/InsurerSharePercentage/Rate"),
-    Deduction("Deduction %","/Placing/ContractSection/OtherDeductions/OtherDeductionsPercentage/Rate"),
-    InstCount("Inst. Count","Inst. Count"),
-    LineBasis("Line % Basis","/Placing/ContractSection/LinePercentageBasis"),
-    RiskCode("Risk Code","/Placing/ContractSection/ContractMarket/PremiumRegulatoryAllocationScheme/Allocation/AllocationCode"),
-    Name("Name","/Placing/CedentDescription ", "/Placing/InsuredDescription");
+    ORD("Ord %","/Placing/ContractSection/BrokerSharePercentage/Rate"),
+    LINE("Line %","/Placing/ContractSection/ContractMarket/InsurerSharePercentage/Rate"),
+    DEDUCTION("Deduction %","/Placing/ContractSection/OtherDeductions/OtherDeductionsPercentage/Rate"),
+    INST_COUNT("Inst. Count","Inst. Count"),
+    LINE_BASIS("Line % Basis","/Placing/ContractSection/LinePercentageBasis"),
+    RISK_CODE("Risk Code","/Placing/ContractSection/ContractMarket/PremiumRegulatoryAllocationScheme/Allocation/AllocationCode"),
+    NAME("Name","/Placing/CedentDescription ", "/Placing/InsuredDescription");
 
     Doc(String label, String... xPath) {
         this.label = label;
@@ -53,8 +53,8 @@ public enum Doc {
     }
 
 }
-//сделай большими названия в enum
-//добавить в doc первую строку для имя
-//добавить что бы проходило всю мапу
-//отдавать map(str,str)
-//переделать алгоритм, сделать его более уневерсальным
+//сделай большими названия в enum +
+//добавить в doc первую строку для имя -
+//добавить что бы проходило всю мапу   -
+//отдавать map(str,str) -
+//переделать алгоритм, сделать его более уневерсальным -
