bf9dbeec3b8a6b41d0578fa54b9b462bf770aed5
vadimka <zavertalyuk.v@gmail.com>
2017-10-26 11:28:31 +0300

    trash

--- src/main/java/com/trainee/demo/Main.java
+++ src/main/java/com/trainee/demo/Main.java
@@ -1,9 +1,11 @@
 package com.trainee.demo;
 
+import org.jdom2.Attribute;
 import org.jdom2.Document;
 import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
+import org.jdom2.xpath.XPath;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
@@ -11,50 +13,57 @@ import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-
-import static com.trainee.demo.Finder.*;
 
 public class Main {
     public static void main(String[] args) throws JDOMException, IOException, SAXException, ParserConfigurationException, URISyntaxException {
         Map<Doc, String> map = new HashMap();
+
         File inputFile = new File("example.xml");
         SAXBuilder saxBuilder = new SAXBuilder();
         Document document = saxBuilder.build(inputFile);
 
         Element rootElement = document.getRootElement();
-        int i = Doc.values().length;
-        System.out.println(i);
+//        rootElement.getChild("", "");
 
-        for (Doc doc : Doc.values()) {
-            for (int j = 0; j < doc.getxPath().size(); ++j) {
-                List<String> list = Parser.parse(doc.getxPath().get(j));
-                switch (list.size()) {
-                    case 1: {
-                        findRecordForOne(map, rootElement, list, doc);
-                        break;
-                    }
-                    case 2: {
-                        findRecordForTwo(map, rootElement, list, doc);
-                        break;
-                    }
-                    case 3: {
-                        findRecordForThree(map, rootElement, list, doc);
-                        break;
-                    }
-                    case 4: {
-                        findRecordForFour(map, rootElement, list, doc);
-                        break;
-                    }
-                    case 5: {
-                        findRecordForFive(map, rootElement, list, doc);
-                        break;
-                    }
-                }
+        Attribute attr = (Attribute) XPath.selectSingleNode(document, "/Placing/ContractSection/ContractMarket/PremiumRegulatoryAllocationScheme/Allocation/AllocationReference");
+        System.out.println(attr.getValue());
 
-            }
-        }
+//        Element el = new Element("parent").addContent(new Element("child").setAttribute("name", "value"));
+//        System.out.println(XPath.selectSingleNode(new Attribute("foo", "bar"), "position()"));
+
+        XPath path = XPath.newInstance("/schedule/@name");
+        System.out.println(path.valueOf(document));
+
+//        getChild(localName, namespaceUri)
+//        for (Doc doc : Doc.values()) {
+//            for (int j = 0; j < doc.getxPath().size(); ++j) {
+//                List<String> list = Parser.parse(doc.getxPath().get(j));
+//                switch (list.size()) {
+//                    case 1: {
+//                        findRecordForOne(map, rootElement, list, doc);
+//                        break;
+//                    }
+//                    case 2: {
+//                        findRecordForTwo(map, rootElement, list, doc);
+//                        break;
+//                    }
+//                    case 3: {
+//                        findRecordForThree(map, rootElement, list, doc);
+//                        break;
+//                    }
+//                    case 4: {
+//                        findRecordForFour(map, rootElement, list, doc);
+//                        break;
+//                    }
+//                    case 5: {
+//                        findRecordForFive(map, rootElement, list, doc);
+//                        break;
+//                    }
+//                }
+//
+//            }
+//        }
         System.out.println();
         System.out.println();
         System.out.println(map.size());
